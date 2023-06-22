6a21c67baaedfe0c0ecd2ad580ff802887de181c
Brian Ho <brian@MacBook-Pro.local>
2023-06-21 23:58:27 -0700

    install bcrypt and update userController basic login

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,11 +1,34 @@
 const userModel = require('../Models/userModel');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const registerUser = async (req, res) => {
-    
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = await userModel.createUser(req.body.username, hashedPassword);
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 };
 
 const loginUser = async (req, res) => {
-    
+    try {
+        const user = await userModel.getUser(req.body.username);
+
+        if (!user) {
+            return res.status(400).json({ message: "Cannot find user" });
+        }
+
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+            res.json({ accessToken: accessToken });
+        } else {
+            res.status(403).json({ message: "Invalid password" });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 };
 
 export default {registerUser, loginUser}
