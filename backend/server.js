608d0c8089943afb3ec71ea3e5e649e3ac9d551b
Brian <37948148+BiTheDev@users.noreply.github.com>
2023-10-17 13:18:05 -0700

    added Google Book API backend

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import mongoose from 'mongoose';
 import booksRoutes from './Routes/bookRoutes.js';
 import userRoutes from './Routes/userRoutes.js';
 import eventsRoutes from './Routes/eventRoutes.js';
+import axios from 'axios';
 
 // Load environment variables from .env file
 config();
@@ -40,6 +41,17 @@ app.use((err, req, res, next) => {
 app.use('/api/user', userRoutes);
 app.use('/api/books', booksRoutes);
 app.use('/api/events', eventsRoutes);
+app.get('/api/searchBooks', async (req, res) => {
+    const query = req.query.q;
+    const googleBooksApiKey = process.env.GOOGLE_BOOKS_API_KEY;
+    try {
+        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=${googleBooksApiKey}`);
+        res.json(response.data);
+    } catch (error) {
+        console.error("Error fetching books:", error);
+        res.status(500).json({ error: 'Failed to fetch books' });
+    }
+});
 
 const PORT = process.env.PORT || 3001;
 mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology:true})
