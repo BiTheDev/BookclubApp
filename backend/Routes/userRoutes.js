eb08b7c0010507f0d7a32763d647d4427c57e99b
Brian <37948148+BiTheDev@users.noreply.github.com>
2023-10-18 21:34:33 -0700

    added new user routes based on controller

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -2,10 +2,15 @@
 
 import { Router } from 'express';
 const router = Router();
-import { register, login } from '../Controllers/userController.js';
+import { register, login, getUserInfo,updateUser,addCurrentBook,completeBook } from '../Controllers/userController.js';
 
 router.post('/register', register);
 router.post('/login', login);
+router.put('/update', updateUser);
+router.get('/userInfo', getUserInfo);
+router.post('/currentBook', addCurrentBook);
+router.post('/completeBook', completeBook);
+
 
 // ... Add other user-related routes
 
