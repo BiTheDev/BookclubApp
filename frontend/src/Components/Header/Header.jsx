bc8b481d0b97d69db852e4d356fc1954248822fa
Brian <37948148+BiTheDev@users.noreply.github.com>
2023-10-20 11:48:30 -0700

    added token checker for button links

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,29 +1,34 @@
-import React, {useContext} from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { Book } from '@mui/icons-material';
-import { Box } from '@mui/system';
-import { AuthContext } from '../../authContext';
+import React, { useContext } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Book } from "@mui/icons-material";
+import { Box } from "@mui/system";
+import { AuthContext } from "../../authContext";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const { authToken, setAuthToken } = useContext(AuthContext);
   const navigate = useNavigate();
   const handleLogout = () => {
-    localStorage.removeItem('jwtToken');
+    localStorage.removeItem("jwtToken");
     setAuthToken(null);
     navigate("/");
-  }
+  };
 
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
         {/* Logo */}
-        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
           <Book /> {/* You can replace this with your Logo */}
         </IconButton>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
@@ -31,23 +36,31 @@ const Header = () => {
         </Typography>
 
         {/* Navigation Links */}
-        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-          <Button href="/home" color="inherit">Home</Button>
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          {authToken && (
+            <Button href="/home" color="inherit">
+              Home
+            </Button>
+          )}
           <Button color="inherit">Browse Books</Button>
-          <Button color="inherit">Discussion Forums</Button>
+          {authToken && <Button color="inherit">Discussion Forums</Button>}
           <Button color="inherit">About Us</Button>
           <Button color="inherit">Contact</Button>
         </Box>
 
         {/* CTA Buttons */}
-        { authToken ? (
-        <Button variant="contained" color="secondary" onClick={handleLogout}>Logout</Button>
-      ) : (
-        <Button href="/access" variant="contained" color="secondary">Login</Button>
-      )}
+        {authToken ? (
+          <Button variant="contained" color="secondary" onClick={handleLogout}>
+            Logout
+          </Button>
+        ) : (
+          <Button href="/access" variant="contained" color="secondary">
+            Login
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
