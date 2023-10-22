096895e4b92b323d915cfe89f53f7d56671309a4
Brian <37948148+BiTheDev@users.noreply.github.com>
2023-10-22 15:07:34 -0700

    added book routes for google books

--- backend/Routes/bookRoutes.js
+++ backend/Routes/bookRoutes.js
@@ -1,14 +1,33 @@
 // Routes/BooksRoutes.js
 
-import { Router } from 'express';
-import { getAllBooks, getBookById, addBook, updateBook, deleteBook } from '../Controllers/bookController.js';
+import { Router } from "express";
+import {
+  getAllBooks,
+  getBookById,
+  addBook,
+  updateBook,
+  deleteBook,
+  searchBooks,
+  trendingBooks,
+  allBooksList,
+  topRatedBooks,
+} from "../Controllers/bookController.js";
 
 const router = Router();
 
-router.get('/', getAllBooks);
-router.get('/:id', getBookById);
-router.post('/', addBook);
-router.put('/:id', updateBook);
-router.delete('/:id', deleteBook);
+//Google Books
+router.get("/book-search", searchBooks);
+router.get("/trending-books", trendingBooks);
+router.get("/all-list", allBooksList);
+router.get("/top-rated-books", topRatedBooks);
+
+
+//regular books
+
+router.get("/get-regular-books", getAllBooks);
+router.get("/get-book/:id", getBookById);
+router.post("/add-book", addBook);
+router.put("/update-book/:id", updateBook);
+router.delete("/delete-book/:id", deleteBook);
 
 export default router;
